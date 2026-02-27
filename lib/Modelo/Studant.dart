7cfada90c2d84ef38cd694e2e7ceade336647847
sr-gerininho <kkaleatorio048@gmail.com>
2026-02-27 20:01:04 -0300

    Depois de DOIS dias, finalmente atualizações das telas, basicamente um toque feminino nela

--- lib/Modelo/Studant.dart
+++ lib/Modelo/Studant.dart
@@ -1,15 +1,27 @@
 class Aluno {
   final String nome;
   final int serie;
-  final String? foto;
   final String turma;
   final String turno;
+  final String? foto;
+
+  //Dados pedagógicos (PEI)
+  final String? diagnostico;
+  final String? nivelSuporte;
+  final String? objetivos;
+  final String? estrategias;
+  final String? observacoes;
 
   Aluno({
     required this.nome,
-    this.foto,
     required this.serie,
     required this.turma,
     required this.turno,
+    this.foto,
+    this.diagnostico,
+    this.nivelSuporte,
+    this.objetivos,
+    this.estrategias,
+    this.observacoes,
   });
 }
