5c1f3ade194220a0b651bb2e8266905b63bd236c
sr-gerininho <kkaleatorio048@gmail.com>
2026-03-05 16:45:25 -0300

    o erro sumiu, nunca fiquei tão feliz nesse carai

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'Screen/home_screen.dart';
+import 'Screen/Login_Screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Sistema Escolar",
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
